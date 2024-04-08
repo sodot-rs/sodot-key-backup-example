ceaf3cff0f81538c696d12572e6457d1c4b2c508
Divo Kaplan <divokaplan@gmail.com>
2024-04-08 20:15:57 +0300

    Switch build adapter and add ARGs to dockerfile.

--- dockerfile
+++ dockerfile
@@ -1,6 +1,11 @@
 FROM node:20
 ADD . /sodot-key-backup-example
 WORKDIR /sodot-key-backup-example
+ARG NPM_TOKEN
+ENV NPM_TOKEN=${NPM_TOKEN}
 RUN npm i
-EXPOSE 5173
-CMD [ "npm", "run", "dev" , "--", "--host"]
+RUN npm run build
+EXPOSE 4173
+ARG API_KEY
+ENV API_KEY=${API_KEY}
+CMD ["npm" , "run", "preview", "--", "--host"]
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, type Plugin } from 'vite';
 import fs from 'fs/promises';
 import path from 'path';
+import { readFileSync } from 'fs';
+import 'dotenv/config';
 
 // NOTE: https://stackoverflow.com/questions/78095780/web-assembly-wasm-errors-in-a-vite-vue-app-using-realm-web-sdk
 async function wasmMiddleware(): Promise<Plugin> {
@@ -32,6 +34,15 @@ async function wasmMiddleware(): Promise<Plugin> {
   };
 }
 
+const keyPath = process.env.KEY_PATH ? readFileSync(process.env.KEY_PATH) : '';
+const chainPath = process.env.CHAIN_PATH ? readFileSync(process.env.CHAIN_PATH) : '';
+const https = keyPath && chainPath ? { key: keyPath, cert: chainPath } : undefined;
+
 export default defineConfig({
   plugins: [sveltekit(), wasmMiddleware()],
+  server: {
+    https,
+    origin: process.env.ORIGIN,
+    proxy: {},
+  },
 });
